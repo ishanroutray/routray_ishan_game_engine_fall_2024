bb7059415b07441cf980da6d0ed0d791f628d9c0
Ishan <109166447+ishqn@users.noreply.github.com>
2024-03-05 09:21:22 -0800

    added coin bar

--- main.py
+++ main.py
@@ -1,29 +1,44 @@
-# This file was created by: ishan routray
-# added this comment to provr github is listening
-# import libraries and modules
+# This file was created by ishan routray
+'''
+health bar
+coin bar
+moving enemies
+'''
+
+# Import modules
 import pygame as pg
 from settings import *
-from sprites import *
 from random import randint
+from sprites import *
 import sys
 from os import path
 
-
-# Define game class...
+# Creating the game class
 class Game:
-    # Define a special method to init the properties of said class...
+    # Initializer -- sets up the game
     def __init__(self):
-        # init pygame
+        # Initializes pygame
         pg.init()
-        # set size of screen and be the screen
-        self.screen = pg.display.set_mode((WIDTH, HEIGHT))
+        # Settings -- set canvas width, height, and title
+        self.screen = pg.display.set_mode((WIDTH, HEIGHT)) 
         pg.display.set_caption(TITLE)
-        # setting game clock 
+        # Setting up pygame clock
         self.clock = pg.time.Clock()
         self.load_data()
+
     def load_data(self):
         game_folder = path.dirname(__file__)
         self.map_data = []
+        # 'r'     open for reading (default)
+        # 'w'     open for writing, truncating the file first
+        # 'x'     open for exclusive creation, failing if the file already exists
+        # 'a'     open for writing, appending to the end of the file if it exists
+        # 'b'     binary mode
+        # 't'     text mode (default)
+        # '+'     open a disk file for updating (reading and writing)
+        # 'U'     universal newlines mode (deprecated)
+        # below opens file for reading in text mode
+        # with 
         '''
         The with statement is a context manager in Python. 
         It is used to ensure that a resource is properly closed or released 
@@ -33,16 +48,14 @@ class Game:
             for line in f:
                 print(line)
                 self.map_data.append(line)
-
-    # Create run method which runs the whole GAME
+    
     def new(self):
-        print("create new game...")
         self.all_sprites = pg.sprite.Group()
         self.walls = pg.sprite.Group()
         self.coins = pg.sprite.Group()
-        # self.player1 = Player(self, 1, 1)
-        # for x in range(10, 20):
-        #     Wall(self, x, 5)
+        self.mobs = pg.sprite.Group()
+        self.powerups = pg.sprite.Group()
+        self.mob = pg.sprite.Group()
         for row, tiles in enumerate(self.map_data):
             print(row)
             for col, tile in enumerate(tiles):
@@ -51,64 +64,85 @@ class Game:
                     print("a wall at", row, col)
                     Wall(self, col, row)
                 if tile == 'P':
-                    self.player = Player(self, col, row)
+                    self.player1 = Player(self, col, row)
                 if tile == 'C':
                     Coin(self, col, row)
+                if tile == 'U':
+                    PowerUp(self, col, row)
+                if tile == 'M':
+                    Mob(self,col,row )
+                
 
+    def draw(self):
+        self.screen.fill(BGCOLOR)
+        self.draw_grid()
+        self.all_sprites.draw(self.screen)
+        self.draw_text(self.screen, "Coins " + str(self.player1.moneybag), 24, WHITE, WIDTH/2 - 32, 2)
+        pg.display.update()
+        pg.display.flip()
+
+    # Runs our game
     def run(self):
-        # 
+        # Game loop while playing
         self.playing = True
         while self.playing:
             self.dt = self.clock.tick(FPS) / 1000
             self.events()
             self.update()
             self.draw()
+
     def quit(self):
-         pg.quit()
-         sys.exit()
+        pg.quit()
+        sys.exit()
 
     def update(self):
+        # Update sprites
         self.all_sprites.update()
-    
+
     def draw_grid(self):
-         for x in range(0, WIDTH, TILESIZE):
-              pg.draw.line(self.screen, LIGHTGREY, (x, 0), (x, HEIGHT))
-         for y in range(0, HEIGHT, TILESIZE):
-              pg.draw.line(self.screen, LIGHTGREY, (0, y), (WIDTH, y))
+        # Vertical lines
+        for x in range(0, WIDTH, TILESIZE):
+            pg.draw.line(self.screen, LIGHTGREY, (x, 0), (x, HEIGHT))
+        # Horizontal lines
+        for y in range(0, WIDTH, TILESIZE):
+            pg.draw.line(self.screen, LIGHTGREY, (0, y), (WIDTH, y))    
+
     def draw_text(self, surface, text, size, color, x, y):
         font_name = pg.font.match_font('arial')
         font = pg.font.Font(font_name, size)
         text_surface = font.render(text, True, color)
         text_rect = text_surface.get_rect()
-        text_rect.topleft = (x*TILESIZE,y*TILESIZE)
+        text_rect.topleft = (x,y)
         surface.blit(text_surface, text_rect)
-    def draw(self):
-            self.screen.fill(BGCOLOR)
-            self.draw_grid()
-            self.all_sprites.draw(self.screen)
-            self.draw_text(self.screen, str(self.player.moneybag), 64, WHITE, 1, 1)
-
-            pg.display.flip()
 
     def events(self):
-         for event in pg.event.get():
+        # Loop through pygame events
+        for event in pg.event.get():
             if event.type == pg.QUIT:
                 self.quit()
+            # If user presses key
             # if event.type == pg.KEYDOWN:
-            #     if event.key == pg.K_LEFT:
-            #         self.player.move(dx=-1)
-            #     if event.key == pg.K_RIGHT:
-            #         self.player.move(dx=1)
-            #     if event.key == pg.K_UP:
-            #         self.player.move(dy=-1)
-            #     if event.key == pg.K_DOWN:
-            #         self.player.move(dy=1)
+            #     # Check input and move character based on input
+            #     match event.key:
+            #         # Move left
+            #         case pg.K_a | pg.K_LEFT:
+            #             self.player1.move(dx=-1)
+            #         # Move right
+            #         case pg.K_d | pg.K_RIGHT:
+            #             self.player1.move(dx=1)
+            #         # Move down
+            #         case pg.K_s | pg.K_DOWN:
+            #             self.player1.move(dy=1)
+            #         # Move up
+            #         case pg.K_w | pg.K_UP:
+            #             self.player1.move(dy=-1)
 
-# Instantiate the game... 
+# Create a new game
 g = Game()
-# use game method run to run
+# Run the game
 # g.show_start_screen()
 while True:
     g.new()
     g.run()
     # g.show_go_screen()
+g.run()
